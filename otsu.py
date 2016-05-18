88d1a31eecd698de77117b3d64a1fca009af143b
thera <fangsai612@163.com>
2016-05-19 05:01:37 +0800

    Revert "the result can be showed on the label2. The problem is  the pic’s size"
    This reverts commit 7b32028250601431a5c5dbe4ac896057b4537a09.

--- otsu.py
+++ otsu.py
@@ -135,7 +135,7 @@ class Window(QtGui.QMainWindow):
         #result button
         btn.setStyleSheet("QPushButton{color: rgb(255, 255, 255);background-color: rgb(167, 205, 255);border:none;padding: 3px;font-family: 'Verdana';font-size: 15px;text-align: center;}"
                           "QPushButton:hover, QPushButton:pressed , QPushButton:checked{background-color: rgb(85, 170, 255);text-align: right;padding-right: 20px;font-weight:100}"
-                          "QPushButton:hover{background-image: url(:next.png);background-repeat:no-repeat;background-position: center left;}"
+                          "QPushButton:hover{background-image: url(:play.png);background-repeat:no-repeat;background-position: center left;}"
                           "QPushButton:pressed, QPushButton:checked{background-image: url(:brightness-3.png);background-repeat:no-repeat;background-position: center left;}")
 
         extractAction = QtGui.QAction(QtGui.QIcon('apple.png'), 'Flee the Scene', self)
@@ -151,7 +151,7 @@ class Window(QtGui.QMainWindow):
         #choose the font style
 
         checkBox = QtGui.QCheckBox('Enlarge', self)
-        checkBox.move(300, 7)
+        checkBox.move(300, 10)
         checkBox.stateChanged.connect(self.enlarge_window)
         #enlarge the window when choose the checkbox
 
@@ -173,68 +173,23 @@ class Window(QtGui.QMainWindow):
         '''end the GUI style'''
 
         self.show()
-
-
-    def pil2pixmap(self,im):
-        if im.mode == "RGB":
-            pass
-        #elif im.mode =="L":
-            #im = im.convert("RGBA")
-        data = im.convert("RGBA").tostring("raw","RGBA")
-        qim = QtGui.QImage(data, im.size[0], im.size[1], QtGui.QImage.Format_ARGB32)
-        pixmap = QtGui.QPixmap.fromImage(qim)
-        return pixmap
-
-
-
-    def resize(self, w, h, w_box, h_box, pil_image):
-        '''
-        resize a pil_image object so it will fit into
-        a box of size w_box times h_box, but retain aspect ratio
-        '''
-        f1 = 1.0*w_box/w # 1.0 forces float division in Python2
-        f2 = 1.0*h_box/h
-        factor = min([f1, f2])
-        #print(f1, f2, factor) # test
-        # use best down-sizing filter
-        width = int(w*factor)
-        height = int(h*factor)
-        return pil_image.resize((width + 3, height + 3), Image.ANTIALIAS)
-    
+        
 
     '''fuction'''    
 
     def rgb2gray(self):
         global img_gray, img_raw
  
+ 
         img_gray = cv2.cvtColor(img_raw, cv2.COLOR_BGR2GRAY)
         #image: RGB2Gray
-
         gray_show = Image.fromarray(img_gray)
-        
-        w,h = gray_show.size
-        ir = self.resize(w, h, 480, 360, gray_show)
-   
-        #image = gray_show
-
-        #frame = img_gray
-
-       
-        #height, width = frame.shape[:2]
-        #img = QtGui.QImage(frame, width, height, QtGui.QImage.Format_RGB888)
-        #img = QtGui.QPixmap.fromImage(img)
-        #self.label2.setPixmap(img)
-
-        self.label2.setPixmap(self.pil2pixmap(ir))
-
-
-        #gray_show = Image.fromarray(img_gray)
-        #image = cv2.cvtColor(gray_show, cv2.COLOR_BGR2RGB)
-        #image = QtGui.QImage(image.data, img_gray.shape[1], image.shape[0], QtGui.QImage.Format_RGB888)
-        #print image
-        #ipixmap = QtGui.QPixmap.fromImage(gray_show)
-        #scaled_pixmap = ipixmap.scaled(self.label2.size())
-        #self.label2.setPixmap(scaled_ipixmap) 
+        image = cv2.cvtColor(gray_show, cv2.COLOR_BGR2RGB)
+        image = QtGui.QImage(image.data, img_gray.shape[1], image.shape[0], QtGui.QImage.Format_RGB888)
+        #print type(image)
+        ipixmap = QtGui.QPixmap.fromImage(image)
+        scaled_pixmap = ipixmap.scaled(self.label2.size())
+        self.label2.setPixmap(scaled_ipixmap) 
 
 
     def iMFilter(self):
@@ -242,10 +197,13 @@ class Window(QtGui.QMainWindow):
         img_otsu =  cv2.medianBlur(img_gray, 7)
         img_gray = img_otsu
         img_otsu = Image.fromarray(img_otsu)
-        
-        w,h = img_otsu.size
-        ir = self.resize(w, h, 480, 360, img_otsu)
-        self.label2.setPixmap(self.pil2pixmap(ir))
+
+        image = cv2.cvtColor(img_otsu, cv2.COLOR_BGR2RGB)
+        image = QtGui.QImage(image.data, img_gray.shape[1], image.shape[0], QtGui.QImage.Format_RGB888)
+
+        ipixmap = QtGui.QPixmap.fromImage(image)
+        scaled_pixmap = ipixmap.scaled(self.label2.size())
+        self.label2.setPixmap(scaled_pixmap) 
 
 
     def otsu(self):
@@ -256,11 +214,14 @@ class Window(QtGui.QMainWindow):
         img_gray = img_otsu
         img_otsu = Image.fromarray(img_otsu)
         
-        w,h = img_otsu.size
-        ir = self.resize(w, h, 480, 360, img_otsu)
-        self.label2.setPixmap(self.pil2pixmap(ir))
-         
+        image = cv2.cvtColor(img_otsu, cv2.COLOR_BGR2RGB)
+        image = QtGui.QImage(image.data, img_gray.shape[1], image.shape[0], QtGui.QImage.Format_RGB888)
+
+        ipixmap = QtGui.QPixmap.fromImage(image)
+        scaled_pixmap = ipixmap.scaled(self.label2.size())
+        self.label2.setPixmap(scaled_pixmap) 
                 
+        
     def Change(self):
         global img_gray
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3, 3))
@@ -268,9 +229,14 @@ class Window(QtGui.QMainWindow):
         img_gray = img_otsu
         img_otsu = Image.fromarray(img_otsu)
 
-        w,h = img_otsu.size
-        ir = self.resize(w, h, 480, 360, img_otsu)      
-        self.label2.setPixmap(self.pil2pixmap(ir))
+        image = cv2.cvtColor(img_otsu, cv2.COLOR_BGR2RGB)
+        image = QtGui.QImage(image.data, img_gray.shape[1], image.shape[0], QtGui.QImage.Format_RGB888)
+
+        ipixmap = QtGui.QPixmap.fromImage(image)
+        scaled_pixmap = ipixmap.scaled(self.label2.size())
+        self.label2.setPixmap(scaled_pixmap)
+
+
 
     def result_show(self):
         global img_gray,img_raw
@@ -280,10 +246,12 @@ class Window(QtGui.QMainWindow):
         #cv2.waitKey(0) 
         img_show = Image.fromarray(img_raw)
 
-        w,h = img_show.size
-        ir = self.resize(w, h, 480, 360, img_show)      
-        self.label2.setPixmap(self.pil2pixmap(ir))
-        
+        image = cv2.cvtColor(img_otsu, cv2.COLOR_BGR2RGB)
+        image = QtGui.QImage(image.data, img_gray.shape[1], image.shape[0], QtGui.QImage.Format_RGB888)
+
+        ipixmap = QtGui.QPixmap.fromImage(image)
+        scaled_pixmap = ipixmap.scaled(self.label2.size())
+        self.label2.setPixmap(scaled_pixmap)
 
 
     '''end function'''
@@ -327,7 +295,7 @@ class Window(QtGui.QMainWindow):
 
     def enlarge_window(self, state):
         if state == QtCore.Qt.Checked:
-            self.setGeometry(50, 50, 1300, 620)
+            self.setGeometry(50,50, 1300, 620)
         else:
             self.setGeometry(50, 50, 500, 300)
         
